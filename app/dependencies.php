b6054f492921fc7d71d97ff8092a3055fec4d5e7
Phillip Robinson <phillip.robinson.nm@gmail.com>
2025-04-04 15:40:48 +0000

    disable twig cache if dev env

--- app/dependencies.php
+++ app/dependencies.php
@@ -4,6 +4,8 @@ use Slim\Views\Twig;
 
 // Set view in Container
 $container->set(Twig::class, function() {
-    return Twig::create(__DIR__ . '/../templates', ['cache' => __DIR__ . '/../cache']);
+    $cache = $_ENV['APP_ENV'] === 'development' ? false : __DIR__ . '/../cache';
+    
+    return Twig::create(__DIR__ . '/../templates', ['cache' => $cache]);
 });
 
